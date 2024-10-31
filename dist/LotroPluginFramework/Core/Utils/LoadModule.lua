5aef1971dd0c0d18f731b1299817c7af730c1bfd
Ziegmaster <trueziegmaster@gmail.com>
2024-10-31 11:00:46 +0500

    Module load optimization
    LPF will now load modules base with first loaded module

--- dist/LotroPluginFramework/Core/Utils/LoadModule.lua
+++ dist/LotroPluginFramework/Core/Utils/LoadModule.lua
@@ -1,4 +1,7 @@
 LPF.LoadModule = function(module_name)
+	if LPF.Modules == nil then
+		import("LotroPluginFramework.Modules")
+	end
 	import("LotroPluginFramework.Modules." .. module_name)
 	LPF.Shell.DebugMessage(LPF.Texts.LPF_Debug.ModuleLoaded(LPF.Dict.ModuleImports[module_name]))
 end
--- dist/LotroPluginFramework/Modules/UI/__init__.lua
+++ dist/LotroPluginFramework/Modules/UI/__init__.lua
@@ -1,7 +1,3 @@
-if LPF.Modules == nil then
-	LPF.Shell.Error('Failed to load the module. Please import "' .. "LotroPluginFramework.Modules" .. '" package first')
-end
-
 LPF.Modules.UI = {
 	Fonts = {
 		Common = {
